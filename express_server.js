73099139bdcf5602fb58cba0d28bef0f482aa737
XiaoyongXu <xxy1994212@gmail.com>
2019-01-16 01:28:23 +0000

    finish w1d2 work

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -31,11 +34,43 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase[req.params]!==undefined){
+    let longURL = urlDatabase[req.params.shortURL];
+    // console.log(longURL, req.params);
+
+    res.redirect(longURL);
+  }else{
+    res.send('not vaild');
+  }
+  
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);  // debug statement to see POST parameters
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(urlDatabase);  
+  res.send(req.body.longURL);         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  const list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
+  let random ="";
+  for (let i = 0; i < 6; i++){
+    random += list[Math.floor(Math.random() * Math.floor(62))]
+  }
+  return random;
+}

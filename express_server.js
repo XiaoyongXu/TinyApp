eed36e10b3500c0c62ef4f27acdd48f457263d6c
XiaoyongXu <xxy1994212@gmail.com>
2019-01-21 00:04:33 +0000

    Final edition

--- express_server.js
+++ express_server.js
@@ -1,20 +1,10 @@
-var cookieSession = require('cookie-session')
-var express = require("express");
+const cookieSession = require('cookie-session')
+const express = require("express");
 const bcrypt = require('bcrypt');
-var app = express();
-var PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs")
-
+const app = express();
+const PORT = 8080; 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1'],
 
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
 
 let urlDatabase = { 
   "test01":{
@@ -31,16 +21,22 @@ let urlDatabase = {
   }
 }
 
-// function urlsForUser(userId){
-//   let urlsForUser = {};
-//   for (element in urlDatabase){
-//     if (urlDatabase[element].id === userID){
-//       urlsForUser[element] = urlDatabase[element];
-//     }
-//   }
-//   return urlsForUser;
-// }
+let users = { 
+  "tony258": {
+    id:"tony258",
+    email: "tonyx258@foxmail.com",
+    password: "123"
+  }
+}
 
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1'],
+  maxAge: 24 * 60 * 60 * 1000 
+}))
 
 app.use(function(req, res, next){
   let loginUrls = {};
@@ -55,38 +51,12 @@ app.use(function(req, res, next){
   next();
 });
 
-let users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "123"
-  },
-  "tony258": {
-    id:"tony258",
-    email: "tonyx258@foxmail.com",
-    password: "123"
-  }
-}
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/hello", (req, res) => {
-  res.render("urls_index");
 });
 
 app.get("/urls", (req, res) => {
@@ -104,9 +74,6 @@ app.get("/urls/new", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("user_login");
 });
-
-
-
 
 app.get("/urls/:id", (req, res) => {
   if (req.session.user_id){
@@ -141,14 +108,14 @@ app.post("/urls", (req, res) => {
     url: req.body.longURL
   }
   
-  res.redirect("/urls/" + shortU);        // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls/");      
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   let i = req.params.id;
   delete urlDatabase[i];
   
-  res.redirect("/urls/");         // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls/");
 });
 
 app.post("/urls/:id", (req, res) => {
@@ -162,7 +129,7 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post('/logout', function (req, res) {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect("/urls");
 });
 
@@ -175,11 +142,10 @@ app.post("/login", (req, res) => {
     let foundUser;
     //check it with the database
     for(var key in users){
-        if(users[key].email===req.body.email && bcrypt.compareSync(users[key].password,req.body.password)){
-          flag = true;
-          foundUser = users[key];
-          
-        }
+      if(users[key].email === req.body.email && bcrypt.compareSync(req.body.password,users[key].password)){
+        flag = true;
+        foundUser = users[key];
+      }
     }
     if(flag){
       req.session.user_id = foundUser.id;
@@ -188,7 +154,6 @@ app.post("/login", (req, res) => {
     } else{
       res.send("please enter the correct userID and pwd")
     }
-    //checking of the username and password from db ends here.
   }
 });
 
@@ -205,12 +170,12 @@ app.post("/register", (req, res) => {
   }
   if(req.body.email === "" ||req.body.password === ""){
     res.statusCode = 400;
-    res.send("err 400");
+    res.send("err 400 please enter vaild email and password");
   } else if(sameEmail){
     res.statusCode = 400;
-    res.send("err 400");
+    res.send("err 400 same email is alread registered");
   } else{
-    users[user_id] ={
+    users[user_id] = {
       id: user_id,
       email: req.body.email,
       password: hashedPassword
@@ -227,7 +192,7 @@ app.listen(PORT, () => {
 
 function generateRandomString() {
   const list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
-  let random ="";
+  let random = "";
   for (let i = 0; i < 6; i++){
     random += list[Math.floor(Math.random() * Math.floor(62))]
   }
